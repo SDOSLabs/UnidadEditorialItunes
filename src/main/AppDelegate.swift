9834fb57bcd7eb151b76b203d5ad5a39938983c9
Rafael Fernández <rafael89.fernandez@gmail.com>
2019-06-12 18:53:58 +0200

    Remove unnecesary code

--- src/main/AppDelegate.swift
+++ src/main/AppDelegate.swift
@@ -15,8 +15,6 @@ class AppDelegate: PluggableApplicationDelegate {
         return [
             EnviromentService.shared, //MUST BE FIRST
             FLEXService.shared,
-            FabricService.shared,
-            FirebaseService.shared,
             KeyboardService.shared,
             UserInterfaceService.shared
         ]
